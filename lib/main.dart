4dc9dbc6dca31bc54927df04cc2cdf3e1f342ae7
RenetaT <153417819+RenetaT@users.noreply.github.com>
2025-02-19 18:55:27 +0000

    create home_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,64 +16,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepOrange,
-      ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          final isWideScreen = constraints.maxWidth > 600;
-          final crossAxisCount = isWideScreen ? 2 : 1;
-
-          return GridView.count(
-            crossAxisCount: crossAxisCount,
-            childAspectRatio: 3,
-            children: _buildCards(),
-          );
-        },
-      ),
-    );
-  }
-
-  List<Widget> _buildCards() {
-    return [
-      Card(
-        child: ListTile(
-          onTap: () {
-            print('Card tapped');
-          },
-          title: Text("Maths"),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          onTap: () {
-            print("Clicked Chemistry");
-          },
-          title: Text("Chemistry"),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          onTap: () {
-            print("Clicked Physics");
-          },
-          title: Text("Physics"),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          onTap: () {
-            print("Clicked Computer Science");
-          },
-          title: Text("Computer Science"),
-        ),
-      ),
-    ];
-  }
-}

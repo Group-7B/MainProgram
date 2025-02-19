255bf612ea686a3deee560a7136ea7af9730544b
RenetaT <153417819+RenetaT@users.noreply.github.com>
2025-02-19 17:11:10 +0000

    basic tables

---
+++ Database.sql
@@ -0,0 +1,82 @@
+---learning App database 
+
+
+---CREATE DATABASE quizz
+
+--- CREATE user table 
+
+CREATE TABLE Users(
+    user_id PRIMERY KEY SERIAL,
+    user_name VARCHAR(20),
+    user_last_name VARCHAR(20),
+    user_email UNIQUE NOT NULL VARCHAR(20),
+    user_paswword VARCHAR(12),
+    join_date TIMESTAMP 
+);
+
+---rewards
+
+--CREATE TABLE Subjects
+CREATE TABLE  Subjects(
+    subject_id SERIAL PRIMARY KEY,
+    subject_name VARCHAR(20),
+    subject_description TEXT
+);
+
+/*One topic can have many quizzes */
+--CTEARE TABLE FOR TOPICS
+CREATE TABLE Topics(
+    topic_id SERIAL PRIMARY KEY,
+    topic_name VARCHAR(20),
+    topic_description TEXT,
+    subject_id INT,  --FOREIGN KEY
+    FOREIGN KEY (subject_id) REFERENCES Subjects(subject_id)
+);
+
+
+
+---CREATE ENIUM FOR QUIZ LEVEL
+CREATE TYPE quiz_level AS ENUM('easy', 'medium', 'hard');
+
+---CREATE ENUM FOR QUIZ TYPE
+CREATE TYPE quiz_type AS ENUM('quizz', 'MOCK EXAM');
+
+--CRETAE table for quizzes 
+CREATE TABLE quizzes (
+    quizz_id SERIAL PRIMARY KEY,
+    quizz_name VARCHAR(20),
+    quizz_description TEXT,
+    quizz_date TIMESTAMP,
+    quizz_type ENUM('quizz', 'MOCK EXAM'),
+    topic_id INT,  --FOREIGN KEY
+    FOREIGN KEY (topic_id) REFERENCES Topics(topic_id) 
+     quiz_level ENUM('easy', 'medium', 'hard')
+);
+
+
+---CREATE table for questions
+CREATE TABLE Questions(
+    question_id SERIAL PRIMARY KEY,
+    question_text TEXT,
+    
+);
+
+
+---CREATE table for answers
+CREATE TABLE Answers(
+    answer_id SERIAL PRIMARY KEY,
+    answer_text TEXT,
+    is_correct BOOLEAN
+    
+);
+
+
+---CREATE table for quiz_questions_answers
+CREATE TABLE quiz_questions_answers(
+    quizz_id INT,  --FOREIGN KEY
+    question_id INT,  --FOREIGN KEY
+    answer_id INT,  --FOREIGN KEY
+    FOREIGN KEY (quizz_id) REFERENCES quizzes(quizz_id),
+    FOREIGN KEY (question_id) REFERENCES Questions(question_id),
+    FOREIGN KEY (answer_id) REFERENCES Answers(answer_id)
+);
